9a052e44202ce5644ac8614864be1b9c00c5c832
me@jeffscudder.com <me@jeffscudder.com@dd86b584-9042-11dd-8fc8-6b048962632c>
2008-10-18 08:31:06 +0000

    The command line Twitter client now displays five updates at a time.

---
+++ trunk/simple_releases/sippy_twitter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ../sippycode/twitter/sippy_twitter.py sippy_twitter/
+cp ../sippycode/twitter/__init__.py sippy_twitter/sippycode/twitter/__init__.py
+cp ../sippycode/twitter/core.py sippy_twitter/sippycode/twitter/core.py
+cp ../sippycode/auth/__init__.py sippy_twitter/sippycode/auth/__init__.py
+cp ../sippycode/auth/core.py sippy_twitter/sippycode/auth/core.py
+cp ../sippycode/http/__init__.py sippy_twitter/sippycode/http/__init__.py
+cp ../sippycode/http/core.py sippy_twitter/sippycode/http/core.py
+zip -r sippy_twitter-0.1.zip sippy_twitter

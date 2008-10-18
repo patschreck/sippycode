0505c1da640d0745517f0686b9c06071dc58a760
me@jeffscudder.com <me@jeffscudder.com@dd86b584-9042-11dd-8fc8-6b048962632c>
2008-10-18 08:56:03 +0000

    Adding a missing __init__.py to the release script.

--- trunk/simple_releases/sippy_twitter.sh
+++ trunk/simple_releases/sippy_twitter.sh
@@ -4,6 +4,7 @@ mkdir -p sippy_twitter/sippycode/twitter/
 mkdir -p sippy_twitter/sippycode/auth/
 mkdir -p sippy_twitter/sippycode/http/
 cp ../sippycode/twitter/sippy_twitter.py sippy_twitter/
+cp ../sippycode/__init__.py sippy_twitter/sippycode/__init__.py
 cp ../sippycode/twitter/__init__.py sippy_twitter/sippycode/twitter/__init__.py
 cp ../sippycode/twitter/core.py sippy_twitter/sippycode/twitter/core.py
 cp ../sippycode/auth/__init__.py sippy_twitter/sippycode/auth/__init__.py
